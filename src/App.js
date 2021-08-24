a8a93e907361a6f4c3885bd84cf0671133c764a0
Mahmoudahmf <mahmoud.ahm910@gmail.com>
2021-08-24 23:39:02 +0200

    test push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Header from './Header';
+import Home from './Home'
 
 function App() {
   return (
     <div className="App">
-      <h1> hello </h1>
+
+      <Header/>
+      <Home/>
+    
+      
     </div>
+    
+
   );
 }
 
